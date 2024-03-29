cfdafce3064284913dab96e9d6d3a49a0a4ab6e4
Joshx567 <105055532+Joshx567@users.noreply.github.com>
2024-03-29 17:22:04 -0400

    Update actuador.ino

--- actuador.ino
+++ actuador.ino
@@ -9,14 +9,6 @@ const char *WIFI_PASS = "76486651NL";
 const char *SERVER_ADDRESS = "192.168.0.8"; //YOUR_SERVER_IP_ADDRESS mi maquina 192.168.78.48
 const int SERVER_PORT = 5000;            //YOUR_SERVER_PORT
 
-  byte mac[] = {
-    0xDE,
-    0xED,
-    0xBA,
-    0xFE,
-    0xFE,
-    0xED};
-
 void setup() 
 {
     for (int i = 0; i < 3; i++)
